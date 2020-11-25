295c64363bb84ca956c8e4875e6b60e005cdf5f5
ryanbrowne360 <ryanbrowne360@gmail.com>
2020-11-25 14:00:34 -0500

    server file created

---
+++ server.js
@@ -0,0 +1,21 @@
+// const mongoose = require('mongoose')
+const express = require('express');
+const PORT = process.env.PORT || 8080;
+
+const app = express();
+
+// displaying the public html
+app.use( express.static('public') );
+
+// parse incoming data for POST
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+//connecting to the route files
+require('./application/router/api_routes.js')(app)
+require('./application/router/html_routes.js')(app)
+
+//initiating server to listen for requests
+app.listen(PORT, function() {
+    console.log("Server live on: http://localhost:" + PORT);
+}); 
