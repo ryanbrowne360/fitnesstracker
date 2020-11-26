bcbe4af573f32f3829374c6189e0be9450c144fb
ryanbrowne360 <ryanbrowne360@gmail.com>
2020-11-25 22:03:49 -0500

    deploying

--- models/index.js
+++ models/index.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 mongoose.connect(
-    process.env.MONGODB_URI || `mongodb://localhost:27017/${process.env.DB_NAME}`,
+    process.env.MONGODB_URI || "mongodb://localhost/workout",
     {
         useNewUrlParser: true,
         useUnifiedTopology: true,
